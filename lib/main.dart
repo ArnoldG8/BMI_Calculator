9bc82c35abd5b48406aa8ee00a99d2e967b12836
Arnold <arnoldaijuka@gmail.com>
2024-02-24 03:00:44 +0300

    Added basic boilerplate structure of BMI system, Code runs

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,25 +28,16 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'BMI Calculator'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +46,131 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+  // int _counter = 10;
+  String _bmi_value = '';
+  // double _sliderValue = 50.0;
+  // String _selectedValue = '';
+  
+  int _calculateBMI(){
+    //BMI formula is kg/m^2
+    double height = double.parse(_heightController.text);
+    double weight = double.parse(_weightController.text);
+    return (weight / (height * height)).round();
+  }
+  void _bmiCategory(){
+    int bmi = _calculateBMI();
+    if(bmi < 18.5){
+      setState(() {
+      _bmi_value = 'Underweight';
     });
+    } else if(bmi >= 18.5 && bmi <= 24.9){
+      setState(() {
+      _bmi_value = 'Normal weight';
+    });
+    } else if(bmi >= 25 && bmi <= 29.9){
+      setState(() {
+      _bmi_value = 'Overweight';
+    });
+    } else if(bmi >= 30){
+      setState(() {
+      _bmi_value = 'Obesity';
+    });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
+              // SizedBox(
+              //   width: 200,
+              //   child: Slider(
+              //   min: 0,
+              //   max: 200,
+              //   value: _sliderValue, 
+              //   onChanged: (value){
+              //     setState(() {
+              //       _sliderValue = value;
+              //     });
+              //   },
+              //   ),
+              // ),
+                // Text(
+                //   '$_sliderValue',
+                //   style: Theme.of(context).textTheme.headlineMedium,
+                // ),
+                SizedBox(
+                  height: 100,
+                  width: 200,
+                  child: TextField(
+                    controller: _heightController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Height in metres'
+                    ),
+                  )
+                ),
+                SizedBox(
+                  height: 100,
+                  width: 200,
+                  child: TextField(
+                    controller: _weightController,
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Weight in kilograms'
+                    ),
+                  )
+                ),
+              // SizedBox(
+              //   height: 60,
+              //   width: 200,
+              //   child:   RadioListTile<String>(
+              //       title: const Text('Male'),
+              //       value: 'Male',
+              //       groupValue: _selectedValue,
+              //       onChanged: (value) {
+              //         setState(() {
+              //           _selectedValue = value!;
+              //     });
+              //     },
+              //   ),
+              // ),
+              // SizedBox(
+              //   height: 60,
+              //   width: 200,
+              //   child:   RadioListTile<String>(
+              //       title: const Text('Female'),
+              //       value: 'Female',
+              //       groupValue: _selectedValue,
+              //       onChanged: (value) {
+              //         setState(() {
+              //           _selectedValue = value!;
+              //     });
+              //     },
+              //   ),
+              // ),
+              TextButton(
+                  onPressed: _bmiCategory,
+                  style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.green,
+                    ),
+                  child: const Text(
+                    'Get BMI Value'
+                  ),
+              ),
+              Text(
+              'Your BMI category is: $_bmi_value',
               style: Theme.of(context).textTheme.headlineMedium,
-            ),
+              ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ), 
     );
   }
 }
